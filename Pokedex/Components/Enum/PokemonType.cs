a7a3eb61c7e48900c5f19812ace5c05c7e866a2f
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-01-13 14:25:19 +0700

    Adding Style Tyle Pokemons

---
+++ Pokedex/Components/Enum/PokemonType.cs
@@ -0,0 +1,25 @@
+﻿namespace Pokedex.Components.Enum
+{
+    public enum PokemonType
+    {
+        None,
+        Normal,
+        Fire,
+        Water,
+        Electric,
+        Grass,
+        Ice,
+        Fighting,
+        Poison,
+        Ground,
+        Flying,
+        Psychic,
+        Bug,
+        Rock,
+        Ghost,
+        Dragon,
+        Dark,
+        Steel,
+        Fairy
+    }
+}
