c3deaa77ebdc6a516b874dd4ad7128891fc0f89c
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-01-14 16:46:19 +0700

    CSS Detail

---
+++ Pokedex/Components/Util/CalculateStat.cs
@@ -0,0 +1,17 @@
+﻿namespace Pokedex.Components.Util
+{
+    public static class CalculateStat
+    {
+        public static int GetMinValue(int baseStat)
+        {
+            // Example calculation for minimum stat value
+            return (int)(baseStat * 0.5);
+        }
+
+        public static int GetMaxValue(int baseStat)
+        {
+            // Example calculation for maximum stat value
+            return (int)(baseStat * 2);
+        }
+    }
+}
