cd310386625949e366c91e639b2045f12fb8c67d
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-01-18 17:12:13 +0700

    Lazy Loading

---
+++ Pokedex/wwwroot/scripts/intersectionObserver.js
@@ -0,0 +1,13 @@
+﻿window.lazyLoad = {
+    observe: function (element, dotNetHelper) {
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    dotNetHelper.invokeMethodAsync('LoadMorePokemon');
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+        observer.observe(element);
+    }
+};
